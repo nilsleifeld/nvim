8dfef61d17bd97af674356f04259bbddc3db41df
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-03-30 16:42:45 +0100

    feat: setup prettier

--- lua/nils/plugins/conform.lua
+++ lua/nils/plugins/conform.lua
@@ -2,19 +2,24 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
-		notify_on_error = false,
+		notify_on_error = true,
 		format_on_save = {
 			timeout_ms = 500,
 			lsp_fallback = true,
 		},
 		formatters_by_ft = {
+			svelte = { { "prettierd", "prettier" } },
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
+			graphql = { { "prettierd", "prettier" } },
+			markdown = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
 			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
-			--
-			-- You can use a sub-list to tell conform to run *until* a formatter
-			-- is found.
-			-- javascript = { { "prettierd", "prettier" } },
+			templ = {},
 		},
 	},
 }
---
+++ lua/nils/plugins/typescript-tools.lua
@@ -0,0 +1,6 @@
+-- TypeScript integration NeoVim deserves
+return {
+	"pmizio/typescript-tools.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	opts = {},
+}
