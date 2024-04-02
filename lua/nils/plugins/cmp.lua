eed83669bab60341a0d4220d6d38df8c52d61c6c
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-04-02 20:25:32 +0200

    feat: add autopairs

--- lua/nils/plugins/cmp.lua
+++ lua/nils/plugins/cmp.lua
@@ -16,6 +16,21 @@ return {
 				return "make install_jsregexp"
 			end)(),
 		},
+		-- autopairing of (){}[] etc
+		{
+			"windwp/nvim-autopairs",
+			opts = {
+				fast_wrap = {},
+				disable_filetype = { "TelescopePrompt", "vim" },
+			},
+			config = function(_, opts)
+				require("nvim-autopairs").setup(opts)
+
+				-- setup cmp for autopairs
+				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+				require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			end,
+		},
 		"saadparwaiz1/cmp_luasnip",
 
 		-- Adds other completion capabilities.
