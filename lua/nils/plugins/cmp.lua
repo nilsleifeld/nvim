273f5a43c979f7bd7e6434a39a114778770178c5
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-04-02 20:44:39 +0200

    feat: add friendly snippets

--- lua/nils/plugins/cmp.lua
+++ lua/nils/plugins/cmp.lua
@@ -6,6 +6,8 @@ return {
 		-- Snippet Engine & its associated nvim-cmp source
 		{
 			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets",
+			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
 			build = (function()
 				-- Build Step is needed for regex support in snippets
 				-- This step is not supported in many windows environments
@@ -15,6 +17,31 @@ return {
 				end
 				return "make install_jsregexp"
 			end)(),
+			config = function()
+				-- vscode format
+				require("luasnip.loaders.from_vscode").lazy_load({ exclude = vim.g.vscode_snippets_exclude or {} })
+				require("luasnip.loaders.from_vscode").lazy_load({ paths = "your path!" })
+				require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.vscode_snippets_path or "" })
+
+				-- snipmate format
+				require("luasnip.loaders.from_snipmate").load()
+				require("luasnip.loaders.from_snipmate").lazy_load({ paths = vim.g.snipmate_snippets_path or "" })
+
+				-- lua format
+				require("luasnip.loaders.from_lua").load()
+				require("luasnip.loaders.from_lua").lazy_load({ paths = vim.g.lua_snippets_path or "" })
+
+				vim.api.nvim_create_autocmd("InsertLeave", {
+					callback = function()
+						if
+							require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+							and not require("luasnip").session.jump_active
+						then
+							require("luasnip").unlink_current()
+						end
+					end,
+				})
+			end,
 		},
 		-- autopairing of (){}[] etc
 		{
