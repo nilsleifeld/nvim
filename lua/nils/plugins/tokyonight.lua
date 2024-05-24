04abf6ca7496cda746230e3c8ce10dd7eff8b93e
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-05-24 09:16:39 +0200

    feat: toggle colorscheme by key

--- lua/nils/plugins/tokyonight.lua
+++ lua/nils/plugins/tokyonight.lua
@@ -1,4 +1,5 @@
 -- Colorscheme
+
 return {
 	-- Change the name of the colorscheme plugin below, and then
 	-- change the command in the config to whatever the name of that colorscheme is
@@ -9,7 +10,22 @@ return {
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
 		-- Load the colorscheme here
-		vim.cmd.colorscheme("tokyonight-night")
+		local dark = false
+
+		local function toggleColorscheme()
+			dark = not dark
+			if dark then
+				vim.cmd.colorscheme("tokyonight-night")
+			else
+				vim.cmd.colorscheme("tokyonight-day")
+			end
+		end
+
+		toggleColorscheme()
+
+		vim.keymap.set("n", "<leader>t", function()
+			toggleColorscheme()
+		end, { desc = "Toggle colorscheme" })
 
 		-- You can configure highlights by doing something like
 		vim.cmd.hi("Comment gui=none")
